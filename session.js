10f8b345373123e76c7d1f7cd04af075e1f28730
Matthew Collier <collier.matthew@gmail.com>
2015-12-03 18:11:50 -0500

    Add global route resolver for session.

--- session.js
+++ session.js
@@ -7,9 +7,20 @@ define(['angular', './session-service'], function(angular, sessionService) {
 
 'use strict';
 
-var module = angular.module('bedrock.session', []);
+var module = angular.module('bedrock.session', ['bedrock.resolver']);
 
 module.service(sessionService);
+
+/* @ngInject */
+module.config(function(routeResolverProvider) {
+  /* @ngInject */
+  routeResolverProvider.add('session', function($route, brSessionService) {
+    if(!$route.current.session) {
+      return false;
+    }
+    return brSessionService.get();
+  });
+});
 
 return module.name;
 
